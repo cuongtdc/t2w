08e2070398fc4ffa058d240c952c844b3acb1d87
cuongtdc <cuongtdc@users.noreply.github.com>
2015-08-26 22:16:15 +0200

    Update JsMetro.js

--- JsMetro.js
+++ JsMetro.js
@@ -21,17 +21,17 @@ function fetchWeather(latitude, longitude) {
                 if (response) {
                   var lati0 = response.data.nearstations[0].lat;
                   var longi0 = response.data.nearstations[0].lon;
-                  var ypos0 = Math.round((lati0 - latitude)*10000)/10;
-                  var xpos0 = Math.round((longi0 - longitude)*10000)/10;
+                  var ypos0 = lati0 - latitude;
+                  var xpos0 = longi0 - longitude;
                   var leng0 = Math.hypot(xpos0,ypos0);
                   for (var i=2; i>=0; i--){
                     var namesta = response.data.nearstations[i].name;
                     var consta = response.data.nearstations[i].connections;
                     var dist = Math.round(response.data.nearstations[i].distance*1000);
-                    var lati = response.data.nearstations[i].lat/leng0;
-                    var longi = response.data.nearstations[i].lon/leng0;
-                    var ypos = Math.round((lati - latitude)*10000)/10;
-                    var xpos = Math.round((longi - longitude)*10000)/10;
+                    var lati = response.data.nearstations[i].lat;
+                    var longi = response.data.nearstations[i].lon;
+                    var ypos = Math.round(((lati - latitude)/leng0)*10000)/10;
+                    var xpos = Math.round(((longi - longitude)/leng0)*10000)/10;
                     Talk2Watch.sendSms(namesta + "\n" + consta + "\n" + dist + " m" + "\n" + "(" + xpos + "," + ypos + ")", "Nearest Metro Station "+ i);
                   }
                 }
