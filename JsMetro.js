0d3166503ce5a927ff2d0d55a33181159f5b7ff4
cuongtdc <cuongtdc@users.noreply.github.com>
2015-08-26 23:07:00 +0200

    Update JsMetro.js

--- JsMetro.js
+++ JsMetro.js
@@ -32,7 +32,8 @@ function fetchWeather(latitude, longitude) {
                     var longi = response.data.nearstations[i].lon;
                     var ypos = Math.round(((lati - latitude)/leng0)*10)/10;
                     var xpos = Math.round(((longi - longitude)/leng0)*10)/10;
-                    var ang = Math.atan2(ypos,xpos)*180/Math.PI;
+                    var ang = Math.round(Math.atan2(ypos,xpos)*180/Math.PI);
+                    if (ang<0) {var ang = 360-ang;}
                     Talk2Watch.sendSms(namesta + "\n" + consta + "\n" + "Polar: (" + dist + " m," + ang + " d)" + "\n" + "(" + xpos + "," + ypos + ")", "Nearest Metro Station "+ i);
                   }
                 }
