1d8f4a8269bbd72c661771da784f7ff0c6e6bb8e
cuongtdc <cuongtdc@users.noreply.github.com>
2015-08-26 14:20:58 +0200

    Update JsWeather.js

--- JsWeather.js
+++ JsWeather.js
@@ -19,10 +19,10 @@ function fetchWeather(latitude, longitude) {
             if (req.status == 200) {
                 response = JSON.parse(req.responseText);
                 if (response) {
-                    var station = response.code;
+                    var station = response.transport;
                     }
 
-                    Talk2Watch.sendSms(station, "Weather in " + code);
+                    Talk2Watch.sendSms(station, "Weather in " + transport);
                 }
             }
         }
@@ -31,48 +31,3 @@ function fetchWeather(latitude, longitude) {
 }
 
 window.navigator.geolocation.getCurrentPosition(locationSuccess,locationError);
-
-/*
-
-function locationSuccess(pos) {
-  var coordinates = pos.coords;
-  fetchWeather(coordinates.latitude, coordinates.longitude);
-}
-
-function locationError(err) {
-
-}
-
-function fetchWeather(latitude, longitude) {
-    var response;
-    var req = new XMLHttpRequest();
-    var url = "http://barcelonaapi.marcpous.com/metro/nearstation/latlon/" + "lat=" + latitude + "/" + longitude + "/1.json";
-//    var url = "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + latitude + "&lon=" + longitude + "&cnt=1";
-    req.open('GET', url, true);
-    req.onload = function(e) {
-
-        if (req.readyState == 4) {
-            if (req.status == 200) {
-                response = JSON.parse(req.responseText);
-                var temperature;
-                if (response) {
-                    var temp = response.main.temp;
-                    var unit = " C";
-                    if (response.sys.country === "US") {
-                        temperature = Math.round(((temp - 273.15) * 1.8) + 32);
-                        unit = " F";
-                    }
-                    else {
-                        temperature = Math.round(temp - 273.15);
-                    }
-
-                    Talk2Watch.sendSms(temperature + unit, "Weather in " + response.name);
-                }
-            }
-        }
-    };
-    req.send(null);
-}
-
-window.navigator.geolocation.getCurrentPosition(locationSuccess,locationError);
-*/
